060255ee7e6959d7950f4802b51f2736a9a2f8d3
Tom Maltese <thomasmaltese64@gmail.com>
2018-02-05 23:47:21 -0500

    fixed increment in h2-3.cc

--- h2/h2-3.cc
+++ h2/h2-3.cc
@@ -10,11 +10,10 @@ int main()
 	int prev, cur, anchor;
 	std::cin >> cur;
 	anchor = prev = cur;
-	int c = 0;
+	int c = 1;
 	while (cur != -1) {
 		std::cin >> cur;
 		if (cur != -1) {
-			c++;
 			if (cur != prev) {
 				if (c % 2 == 0) {
 					anchor = prev = cur;
@@ -24,11 +23,13 @@ int main()
 					prev = anchor;
 					c = 1;
 				}
+			} else {
+				c++;
 			}
 		}
 	}
 	while (c--) {
-		printf("%d ", anchor);
+		printf("%d ", prev);
 	}
 	putchar('\n');
 	return 0;
