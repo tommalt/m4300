d09ea94b33389595c4f50e60c66afb56850fa8dd
thomasmaltese <thomasmaltese64@gmail.com>
2018-03-06 11:34:22 -0500

    working out bugs in h5-3 (unfinished)

--- h5/h5-3.cc
+++ h5/h5-3.cc
@@ -10,38 +10,47 @@
 #include <stdarg.h>
 
 #define ARRAY_SIZE(x) (sizeof x / sizeof x[0])
+#define MIN(x,y) ((x) < (y)) ? (x) : (y)
 
 void show(int *a, int size, char const *msg, ...);
 
 void merge_sort(int *a, int size)
 {
 	int *buf = (int *) malloc(size * sizeof *buf);
-	for (int chunksize = 1; chunksize < 5; chunksize = chunksize * 2) {
+	int maxchunk = (size % 2 == 0) ? size / 2 : (size + 1) / 2;
+	for (int chunksize = 1; chunksize <= maxchunk; chunksize = chunksize * 2) {
+		memset(buf, 0, size * sizeof *buf);
+		printf("[chunksize = %d]\n", chunksize);
 		for (int chunk = 0; chunk < size; chunk += chunksize * 2) {
 			int i, j, k;
 			int L_END, R_END;
 			i = k = chunk;
 			j = i + chunksize;
+			//L_END = MIN(i + chunksize, size);
+			//R_END = MIN(j + chunksize, size);
 			L_END = i + chunksize;
 			R_END = j + chunksize;
-			while (i < L_END) {
-				while (j < R_END && a[j] <= a[i]) {
+			while (i < L_END && i < size) {
+				while (j < R_END && j < size && a[j] <= a[i]) {
+					printf("writing %d to buf[%d]\n", a[j], k);
 					buf[k++] = a[j++];
 				}
+				printf("writing %d to buf[%d]\n", a[i], k);
 				buf[k++] = a[i++];
 			}
-			while (j < R_END)
+			while (j < R_END && j < size) {
+				printf("writing %d to buf[%d]\n", a[j], k);
 				buf[k++] = a[j++];
-			memcpy(a + chunk, buf + chunk, (chunksize * 2) * sizeof *buf);
-			//memcpy(a, buf, sizeof a);
-			//if (chunksize == 1) {
-				//printf("chunk = %d\n", chunk);
-				//show(a, size, "A (c = 1): ");
-			//}
+			}
 		}
-		show(a, size, "after chunksize = %d: ", chunksize);
+		for (int c = 0; c < size; c++) {
+			a[c] = buf[c];
+		}
+		printf("[/chunksize = %d]\n", chunksize);
+		//show(a, size, "after chunksize = %d: ", chunksize);
+		//printf("OK.\n");
 	}
-	//memcpy(a, buf, sizeof a);
+	free(buf);
 }
 
 void show(int *a, int size, char const *msg, ...)
@@ -58,9 +67,10 @@ void show(int *a, int size, char const *msg, ...)
 }
 int main()
 {
-	int A[] = {6, 5, 3, 1, 8, 7, 2, 4, 9};
+	int A[] = {6, 5, 3, 10, 1, 8, 7, 2, 4, 9};
+	//int A[] = {6, 5, 3};
 	int size = ARRAY_SIZE(A);
-	//show(A, size, "Before: ");
+	show(A, size, "Before: ");
 	merge_sort(A, size);
-	//show(A, size, "After : ");
+	show(A, size, "After : ");
 }
